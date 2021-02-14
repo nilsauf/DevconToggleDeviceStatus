88f861d8e24b83e14ad864401c30d2421b04a930
Nils Aufschläger <mail@nilsauf.de>
2021-02-14 12:17:09 +0100

    Add First Version Power Shell Scripts

---
+++ DisableDevice.ps1
@@ -0,0 +1,3 @@
+﻿$DeviceName = "XXX"
+
+Get-PnpDevice | Where-Object { $PSItem.FriendlyName -like $DeviceName -and $PSItem.Status -eq "OK" } | Disable-PnpDevice -Confirm:$false
---
+++ EnableDevice.ps1
@@ -0,0 +1,3 @@
+﻿$DeviceName = "XXX"
+
+Get-PnpDevice | Where-Object { $PSItem.FriendlyName -like $DeviceName -and $PSItem.Status -eq "OK" -or $PSItem.Status -eq "Error" } | Enable-PnpDevice -Confirm:$false
